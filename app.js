ad7f0607945219ce8bcc0b97669a4fb616f14c0f
kemikal <janne@kemikal.org>
2023-03-01 11:55:19 +0100

    user POC

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
 
 var app = express();
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 let users = [
-  {id: 1, name: 'John'},
-  {id: 2, name: 'Jane'},
+  {id: 1, name: 'John', likes: "Odla, Kaniner, långa promenader"},
+  {id: 2, name: 'Jane', password: "test"},
   {id: 3, name: 'Bob'},
-  {id: 4, name: 'Alice'}
+  {id: 4, name: 'Alice'},
+  {id: 5, name: 'Bengt'}
 ]
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.json(users);
+});
+
+router.get('/:userId', function(req, res, next) {
+  userId = req.params.userId;
+  console.log(userId);
+
+  let findUser = users.find(user => user.id == userId);
+
+  res.json(findUser);
+});
+
+router.post('/', function(req, res, next) {
+
+  let newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  console.log(newUser);
+
   res.json(users);
 });
 
