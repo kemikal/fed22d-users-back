1d465771784acd950ecc51d95bdab6ea989e2b7b
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2023-03-10 11:59:25 +0100

    added json file and crypto-js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
+const crypto = require("crypto-js");
+//const salt = "guinea pigs are awesome";
 
 let users = [
   {id: 1, name: 'John', likes: "Odla, Kaniner, långa promenader"},
@@ -11,7 +14,15 @@ let users = [
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json(users);
+  // res.json(users);
+  fs.readFile("users.json", function(err, data){
+    if(err) {
+      console.log(err)
+    }
+    res.send(data)
+    return;
+  }
+  )
 });
 
 router.get('/:userId', function(req, res, next) {
@@ -24,25 +35,47 @@ router.get('/:userId', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-
-  let newUser = req.body;
-  newUser.id = users.length + 1;
-  users.push(newUser);
-  console.log(newUser);
-
-  res.json(users);
+  let newUser = { name: req.body.name };
+  fs.readFile("users.json", function(err, data){
+    if(err) {
+      console.log(err)
+    }
+    let users = JSON.parse(data)
+    newUser.id = users.length + 1;
+    let passwordToSave = crypto.SHA3(req.body.password).toString();
+   // let passwordToSave = crypto.AES.encrypt(req.body.password, salt).toString();
+    newUser.password = passwordToSave;
+    users.push(newUser);
+    fs.writeFile("users.json", JSON.stringify(users, null, 2), function(err) {
+      if(err) {
+        console.log(err)
+      }
+      res.json(users)
+    })
+  })
 });
 
 router.post('/login', function(req, res, next) {
   const { name, password } = req.body;
-  const foundUser = users.find(user => user.name === name);
+  fs.readFile("users.json", function(err, data){
+    if(err) {
+      console.log(err)
+    }
+    let users = JSON.parse(data);
+    const foundUser = users.find(user => user.name === name);
+    if(crypto.SHA3(password).toString() === foundUser.password) {
+   // if(password === crypto.AES.decrypt(foundUser.password, salt).toString(crypto.enc.Utf8)) {
+      res.status(201).json({name: foundUser.name, id: foundUser.id})
+    }
+    else {
+      res.status(401).json("Incorrect password or username")
+    }
+    return;
+  }
+  )
+  
 
-  if(password === foundUser.password) {
-    res.status(201).json({name: foundUser.name, id: foundUser.id})
-  }
-  else {
-    res.status(401).json("Incorrect password or username")
-  }
+  
 });
 
 router.get('/test', function(req, res, next) {
